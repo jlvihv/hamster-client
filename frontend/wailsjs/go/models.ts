09621eea752b15d16a95eef0648841452c4904db
sunjianguo <jianguo.sun@newtouch.com>
2022-07-05 16:17:23 +0800

    remove  Abbreviation  field

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,3 +1,180 @@
+export namespace deploy {
+  export class DeployParams {
+    id: number;
+    nodeEthereumUrl: string;
+    ethereumUrl: string;
+    ethereumNetwork: string;
+    indexerAddress: string;
+    mnemonic: string;
+
+    static createFrom(source: any = {}) {
+      return new DeployParams(source);
+    }
+
+    constructor(source: any = {}) {
+      if ('string' === typeof source) source = JSON.parse(source);
+      this.id = source['id'];
+      this.nodeEthereumUrl = source['nodeEthereumUrl'];
+      this.ethereumUrl = source['ethereumUrl'];
+      this.ethereumNetwork = source['ethereumNetwork'];
+      this.indexerAddress = source['indexerAddress'];
+      this.mnemonic = source['mnemonic'];
+    }
+  }
+}
+
+export namespace application {
+  export class Application {
+    id: number;
+    name: string;
+    describe: string;
+    status: number;
+    // Go type: time.Time
+    createdAt: any;
+    // Go type: time.Time
+    updatedAt: any;
+    // Go type: gorm.DeletedAt
+    deletedAt: any;
+
+    static createFrom(source: any = {}) {
+      return new Application(source);
+    }
+
+    constructor(source: any = {}) {
+      if ('string' === typeof source) source = JSON.parse(source);
+      this.id = source['id'];
+      this.name = source['name'];
+      this.describe = source['describe'];
+      this.status = source['status'];
+      this.createdAt = this.convertValues(source['createdAt'], null);
+      this.updatedAt = this.convertValues(source['updatedAt'], null);
+      this.deletedAt = this.convertValues(source['deletedAt'], null);
+    }
+
+    convertValues(a: any, classs: any, asMap: boolean = false): any {
+      if (!a) {
+        return a;
+      }
+      if (a.slice) {
+        return (a as any[]).map((elem) => this.convertValues(elem, classs));
+      } else if ('object' === typeof a) {
+        if (asMap) {
+          for (const key of Object.keys(a)) {
+            a[key] = new classs(a[key]);
+          }
+          return a;
+        }
+        return new classs(a);
+      }
+      return a;
+    }
+  }
+  export class PageApplicationVo {
+    items: Application[];
+    total: number;
+
+    static createFrom(source: any = {}) {
+      return new PageApplicationVo(source);
+    }
+
+    constructor(source: any = {}) {
+      if ('string' === typeof source) source = JSON.parse(source);
+      this.items = this.convertValues(source['items'], Application);
+      this.total = source['total'];
+    }
+
+    convertValues(a: any, classs: any, asMap: boolean = false): any {
+      if (!a) {
+        return a;
+      }
+      if (a.slice) {
+        return (a as any[]).map((elem) => this.convertValues(elem, classs));
+      } else if ('object' === typeof a) {
+        if (asMap) {
+          for (const key of Object.keys(a)) {
+            a[key] = new classs(a[key]);
+          }
+          return a;
+        }
+        return new classs(a);
+      }
+      return a;
+    }
+  }
+  export class ApplyVo {
+    id: number;
+    // Go type: time.Time
+    createdAt: any;
+    // Go type: time.Time
+    updatedAt: any;
+    name: string;
+    describe: string;
+    status: number;
+
+    static createFrom(source: any = {}) {
+      return new ApplyVo(source);
+    }
+
+    constructor(source: any = {}) {
+      if ('string' === typeof source) source = JSON.parse(source);
+      this.id = source['id'];
+      this.createdAt = this.convertValues(source['createdAt'], null);
+      this.updatedAt = this.convertValues(source['updatedAt'], null);
+      this.name = source['name'];
+      this.describe = source['describe'];
+      this.status = source['status'];
+    }
+
+    convertValues(a: any, classs: any, asMap: boolean = false): any {
+      if (!a) {
+        return a;
+      }
+      if (a.slice) {
+        return (a as any[]).map((elem) => this.convertValues(elem, classs));
+      } else if ('object' === typeof a) {
+        if (asMap) {
+          for (const key of Object.keys(a)) {
+            a[key] = new classs(a[key]);
+          }
+          return a;
+        }
+        return new classs(a);
+      }
+      return a;
+    }
+  }
+  export class UpdateApplicationParam {
+    id: number;
+    name: string;
+    describe: string;
+
+    static createFrom(source: any = {}) {
+      return new UpdateApplicationParam(source);
+    }
+
+    constructor(source: any = {}) {
+      if ('string' === typeof source) source = JSON.parse(source);
+      this.id = source['id'];
+      this.name = source['name'];
+      this.describe = source['describe'];
+    }
+  }
+  export class AddApplicationParam {
+    name: string;
+    describe: string;
+
+    static createFrom(source: any = {}) {
+      return new AddApplicationParam(source);
+    }
+
+    constructor(source: any = {}) {
+      if ('string' === typeof source) source = JSON.parse(source);
+      this.name = source['name'];
+      this.describe = source['describe'];
+    }
+  }
+}
+
 export namespace graph {
   export class GraphParameterVo {
     nodeEthereumUrl: string;
@@ -7,7 +184,6 @@ export namespace graph {
     mnemonic: string;
     applicationId: number;
     name: string;
-    abbreviation: string;
     describe: string;
     status: number;
     // Go type: time.Time
@@ -28,7 +204,6 @@ export namespace graph {
       this.mnemonic = source['mnemonic'];
       this.applicationId = source['applicationId'];
       this.name = source['name'];
-      this.abbreviation = source['abbreviation'];
       this.describe = source['describe'];
       this.status = source['status'];
       this.createdAt = this.convertValues(source['createdAt'], null);
@@ -160,191 +335,6 @@ export namespace wallet {
       if ('string' === typeof source) source = JSON.parse(source);
       this.address = source['address'];
       this.address_json = source['address_json'];
-    }
-  }
-}
-
-export namespace deploy {
-  export class DeployParams {
-    id: number;
-    nodeEthereumUrl: string;
-    ethereumUrl: string;
-    ethereumNetwork: string;
-    indexerAddress: string;
-    mnemonic: string;
-
-    static createFrom(source: any = {}) {
-      return new DeployParams(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) source = JSON.parse(source);
-      this.id = source['id'];
-      this.nodeEthereumUrl = source['nodeEthereumUrl'];
-      this.ethereumUrl = source['ethereumUrl'];
-      this.ethereumNetwork = source['ethereumNetwork'];
-      this.indexerAddress = source['indexerAddress'];
-      this.mnemonic = source['mnemonic'];
-    }
-  }
-}
-
-export namespace application {
-  export class Application {
-    id: number;
-    name: string;
-    abbreviation: string;
-    describe: string;
-    status: number;
-    // Go type: time.Time
-    createdAt: any;
-    // Go type: time.Time
-    updatedAt: any;
-    // Go type: gorm.DeletedAt
-    deletedAt: any;
-
-    static createFrom(source: any = {}) {
-      return new Application(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) source = JSON.parse(source);
-      this.id = source['id'];
-      this.name = source['name'];
-      this.abbreviation = source['abbreviation'];
-      this.describe = source['describe'];
-      this.status = source['status'];
-      this.createdAt = this.convertValues(source['createdAt'], null);
-      this.updatedAt = this.convertValues(source['updatedAt'], null);
-      this.deletedAt = this.convertValues(source['deletedAt'], null);
-    }
-
-    convertValues(a: any, classs: any, asMap: boolean = false): any {
-      if (!a) {
-        return a;
-      }
-      if (a.slice) {
-        return (a as any[]).map((elem) => this.convertValues(elem, classs));
-      } else if ('object' === typeof a) {
-        if (asMap) {
-          for (const key of Object.keys(a)) {
-            a[key] = new classs(a[key]);
-          }
-          return a;
-        }
-        return new classs(a);
-      }
-      return a;
-    }
-  }
-  export class PageApplicationVo {
-    items: Application[];
-    total: number;
-
-    static createFrom(source: any = {}) {
-      return new PageApplicationVo(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) source = JSON.parse(source);
-      this.items = this.convertValues(source['items'], Application);
-      this.total = source['total'];
-    }
-
-    convertValues(a: any, classs: any, asMap: boolean = false): any {
-      if (!a) {
-        return a;
-      }
-      if (a.slice) {
-        return (a as any[]).map((elem) => this.convertValues(elem, classs));
-      } else if ('object' === typeof a) {
-        if (asMap) {
-          for (const key of Object.keys(a)) {
-            a[key] = new classs(a[key]);
-          }
-          return a;
-        }
-        return new classs(a);
-      }
-      return a;
-    }
-  }
-  export class ApplyVo {
-    id: number;
-    // Go type: time.Time
-    createdAt: any;
-    // Go type: time.Time
-    updatedAt: any;
-    name: string;
-    abbreviation: string;
-    describe: string;
-    status: number;
-
-    static createFrom(source: any = {}) {
-      return new ApplyVo(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) source = JSON.parse(source);
-      this.id = source['id'];
-      this.createdAt = this.convertValues(source['createdAt'], null);
-      this.updatedAt = this.convertValues(source['updatedAt'], null);
-      this.name = source['name'];
-      this.abbreviation = source['abbreviation'];
-      this.describe = source['describe'];
-      this.status = source['status'];
-    }
-
-    convertValues(a: any, classs: any, asMap: boolean = false): any {
-      if (!a) {
-        return a;
-      }
-      if (a.slice) {
-        return (a as any[]).map((elem) => this.convertValues(elem, classs));
-      } else if ('object' === typeof a) {
-        if (asMap) {
-          for (const key of Object.keys(a)) {
-            a[key] = new classs(a[key]);
-          }
-          return a;
-        }
-        return new classs(a);
-      }
-      return a;
-    }
-  }
-  export class UpdateApplicationParam {
-    id: number;
-    name: string;
-    abbreviation: string;
-    describe: string;
-
-    static createFrom(source: any = {}) {
-      return new UpdateApplicationParam(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) source = JSON.parse(source);
-      this.id = source['id'];
-      this.name = source['name'];
-      this.abbreviation = source['abbreviation'];
-      this.describe = source['describe'];
-    }
-  }
-  export class AddApplicationParam {
-    name: string;
-    abbreviation: string;
-    describe: string;
-
-    static createFrom(source: any = {}) {
-      return new AddApplicationParam(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) source = JSON.parse(source);
-      this.name = source['name'];
-      this.abbreviation = source['abbreviation'];
-      this.describe = source['describe'];
     }
   }
 }
--- module/application/application.go
+++ module/application/application.go
@@ -6,27 +6,24 @@ import (
 )
 
 type Application struct {
-	ID           uint           `json:"id" gorm:"primarykey"`
-	Name         string         `json:"name"`         //apply name
-	Abbreviation string         `json:"abbreviation"` //apply abbreviation
-	Describe     string         `json:"describe"`     //apply describe
-	Status       int            `json:"status"`       //apply status 0: not deploy 1:deployed 2:ALL
-	CreatedAt    time.Time      `json:"createdAt"`
-	UpdatedAt    time.Time      `json:"updatedAt"`
-	DeletedAt    gorm.DeletedAt `json:"deletedAt" gorm:"index"`
+	ID        uint           `json:"id" gorm:"primarykey"`
+	Name      string         `json:"name"`     //apply name
+	Describe  string         `json:"describe"` //apply describe
+	Status    int            `json:"status"`   //apply status 0: not deploy 1:deployed 2:ALL
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
 }
 
 type AddApplicationParam struct {
-	Name         string `json:"name"`         //apply name
-	Abbreviation string `json:"abbreviation"` //apply abbreviation
-	Describe     string `json:"describe"`     //apply describe
+	Name     string `json:"name"`     //apply name
+	Describe string `json:"describe"` //apply describe
 }
 
 type UpdateApplicationParam struct {
-	ID           uint   `json:"id"`           //application ID
-	Name         string `json:"name"`         //apply name
-	Abbreviation string `json:"abbreviation"` //apply abbreviation
-	Describe     string `json:"describe"`     //apply describe
+	ID       uint   `json:"id"`       //application ID
+	Name     string `json:"name"`     //apply name
+	Describe string `json:"describe"` //apply describe
 }
 
 type PageApplicationVo struct {
@@ -35,18 +32,17 @@ type PageApplicationVo struct {
 }
 
 type ApplyVo struct {
-	ID           uint      `json:"id"`
-	CreatedAt    time.Time `json:"createdAt"`
-	UpdatedAt    time.Time `json:"updatedAt"`
-	Name         string    `json:"name"`         //apply name
-	Abbreviation string    `json:"abbreviation"` //apply abbreviation
-	Describe     string    `json:"describe"`     //apply describe
-	Status       int       `json:"status"`
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Name      string    `json:"name"`     //apply name
+	Describe  string    `json:"describe"` //apply describe
+	Status    int       `json:"status"`
 }
 
 type Service interface {
 	AddApplication(application *AddApplicationParam) (bool, error)
-	UpdateApplication(id int, name string, abbreviation string, des string) (bool, error)
+	UpdateApplication(id int, name string, des string) (bool, error)
 	DeleteApplication(id int) (bool, error)
 	QueryApplicationById(id int) (ApplyVo, error)
 	ApplicationList(page, pageSize int, name string, status int) (PageApplicationVo, error)
