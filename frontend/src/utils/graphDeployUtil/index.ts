879062edc711ff37ffced990cf0e4ae6b2dc7241
sunjianguo <jianguo.sun@newtouch.com>
2022-08-02 16:12:11 +0800

    change rinkeby url

--- frontend/src/utils/graphDeployUtil/index.ts
+++ frontend/src/utils/graphDeployUtil/index.ts
@@ -6,7 +6,7 @@ export const pluginConfigs = [
 
 export const deployData = {
   nodeEthereumUrl: 'mainnet:https://main-light.eth.linkpool.io',
-  ethereumUrl: 'https://eth-rinkeby.alchemyapi.io/v2/PbV0cthp513-kWPjSyhZE3S8gzyhwyoi',
+  ethereumUrl: 'https://rinkeby.infura.io/v3/62d7b5f33ae443e784919f1c2afe24a3',
   ethereumNetwork: 'rinkeby',
   indexerAddress: '',
 };
