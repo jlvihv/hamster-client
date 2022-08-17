a06d006b71d34eb91fb66069d9cf2fe0d8498998
jlvihv <vihv@qq.com>
2022-08-17 17:29:24 +0800

    Added functions for calling graph connect, start, and rules

--- app/graph.go
+++ app/graph.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"hamster-client/module/graph"
 	"hamster-client/module/graph/cli"
+	graphV2 "hamster-client/module/graph/v2"
 )
 
 type Graph struct {
-	ctx          context.Context
-	graphService graph.Service
-	cliService   cli.Service
+	ctx            context.Context
+	graphService   graph.Service
+	cliService     cli.Service
+	graphV2Service graphV2.Service
 }
 
 func NewGraphApp(graphService graph.Service, cliService cli.Service) Graph {
@@ -30,4 +32,21 @@ func (g *Graph) QueryApplyAndParams(applicationId int) (graph.GraphParameterVo, 
 
 func (g *Graph) CliLink(applicationId int) (int, error) {
 	return g.cliService.CliLink(applicationId)
+}
+
+func (g *Graph) GraphConnect() error {
+	return g.graphV2Service.GraphConnect(34003)
+}
+
+func (g *Graph) GraphStart(deploymentID string) error {
+	return g.graphV2Service.GraphStart(34003, deploymentID)
+}
+
+func (g *Graph) GraphRules() (GraphRulesInfo, error) {
+	info, err := g.graphV2Service.GraphRules(34003)
+	return GraphRulesInfo{Info: info}, err
+}
+
+type GraphRulesInfo struct {
+	Info []map[string]interface{} `json:"info"`
 }
