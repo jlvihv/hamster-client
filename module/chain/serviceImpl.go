ce26416da1293adbbadc9f982db51849868d2454
jlvihv <vihv@qq.com>
2022-10-09 11:41:40 +0800

    add chain

---
+++ module/chain/serviceImpl.go
@@ -0,0 +1,89 @@
+package chain
+
+import (
+	"fmt"
+	"hamster-client/config"
+	"hamster-client/module/application"
+	"hamster-client/module/p2p"
+	"hamster-client/module/queue"
+
+	"gorm.io/gorm"
+)
+
+type VersionVo struct {
+	Version string `json:"version"`
+}
+
+type ServiceImpl struct {
+	db  *gorm.DB
+	app application.Service
+	p2p p2p.Service
+	q   queue.Queue
+}
+
+func NewServiceImpl(db *gorm.DB, app application.Service, p2p p2p.Service) ServiceImpl {
+	return ServiceImpl{
+		db:  db,
+		app: app,
+		p2p: p2p,
+	}
+}
+
+func (c *ServiceImpl) getP2pForwardPort(appID int) (p2pForwardPort int, err error) {
+	vo, err := c.app.QueryApplicationById(appID)
+	if err != nil {
+		fmt.Println("query application by id failed, err: ", err)
+		return
+	}
+
+	fmt.Println("pull before: reForwardLink:", vo.PeerId)
+	if _, err = c.p2p.GetSetting(); err != nil {
+		_ = c.p2p.InitSetting()
+	}
+
+	err = reForwardLink(c.p2p, vo.P2pForwardPort, vo.PeerId)
+	if err != nil {
+		fmt.Println("reForwardLink failed, err: ", err)
+		return
+	}
+	return vo.P2pForwardPort, nil
+}
+
+func reForwardLink(p2pService p2p.Service, port int, peerID string) error {
+	protocol := config.ProviderProtocol
+	err := p2pService.LinkByProtocol(protocol, port, peerID)
+	return err
+}
+
+func (c *ServiceImpl) StartQueue(appID int) error {
+	pullImageJob := NewPullImageJob(appID, c)
+	startJob := NewStartJob(appID, c)
+	q, err := queue.NewQueue(appID, c.db, pullImageJob, startJob)
+	if err != nil {
+		return err
+	}
+	c.q = q
+
+	// start queue, in a new goroutine
+	done := make(chan struct{})
+	go q.Start(done)
+
+	// wait
+	<-done
+
+	return nil
+}
+
+type QueueInfo struct {
+	Info []queue.StatusInfo `json:"info"`
+}
+
+func (c *ServiceImpl) GetQueueInfo(appID int) (QueueInfo, error) {
+	info, err := c.q.GetStatus()
+	if err != nil {
+		return QueueInfo{}, err
+	}
+	return QueueInfo{
+		Info: info,
+	}, nil
+}
---
+++ module/chain/serviceImpl_test.go
@@ -0,0 +1,49 @@
+package chain
+
+import (
+	"context"
+	"hamster-client/module/account"
+	"hamster-client/module/application"
+	"hamster-client/module/graph"
+	"hamster-client/module/p2p"
+	"hamster-client/module/resource"
+	"hamster-client/module/wallet"
+	"hamster-client/utils"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/mitchellh/go-homedir"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestStartQueue(t *testing.T) {
+	configPath, _ := homedir.Expand("~/.link/")
+	db, err := gorm.Open(sqlite.Open(filepath.Join(configPath, "link.db")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	err = db.AutoMigrate(
+		&account.Account{},
+		&p2p.P2pConfig{},
+		&resource.Resource{},
+		&wallet.Wallet{},
+		&application.Application{},
+		&graph.GraphParameter{},
+	)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	httpUtil := utils.NewHttp()
+	accountService := account.NewServiceImpl(ctx, db, httpUtil)
+	applicationService := application.NewServiceImpl(ctx, db)
+	p2pService := p2p.NewServiceImpl(ctx, db, &accountService)
+	si := NewServiceImpl(db, &applicationService, &p2pService)
+	err = si.StartQueue(123)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(60 * time.Second)
+}
